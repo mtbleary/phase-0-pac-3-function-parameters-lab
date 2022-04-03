9f5b8a41b0ca63ee79b2dc5d05b45c94fb12050a
matthew leary <matt-leary@live.co.uk>
2022-04-03 21:06:50 +0100

    commit tree dd37885bd0d17ce5ef5f621f9cd24cfdc839738a

---
+++ index.js
@@ -0,0 +1,15 @@
+  function introduction(name) {
+    return `Hi, my name is ${name}.`; 
+  }
+    let name = `Matt`;
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+  let language = `Node.JS`;
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
